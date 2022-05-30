82d472cbdd70465ec99131870194bf68664f8ae0
윤창현 <hhlsdkm@gmail.com>
2022-05-30 10:02:22 +0900

    input default

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,9 +1,16 @@
 import Input from "../../UI/Input";
 import classes from './MealItemForm.module.css'
 
-const MealItemForm = () => {
+const MealItemForm = (props) => {
     return <form className={classes.form}>
-        <Input />
+        <Input label="Amount" input={{
+            id: 'amount',
+            type: 'number',
+            min: '1',
+            max: '5',
+            step: '1',
+            defaultValue: '1'
+        }} />
         <button>+ Add</button>
     </form>
 };
