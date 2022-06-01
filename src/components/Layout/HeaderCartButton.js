530be4aeea9fd8823b2d3c3e8f8c05cc49cc21ee
Sherry90 <hhlsdkm@gmail.com>
2022-06-01 18:08:33 +0900

    Modal open & close

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -2,8 +2,8 @@ import CartIcon from "../Cart/CartIcon";
 
 import classes from "./HeaderCartButton.module.css";
 
-const HeaderCartButton = () => {
-    return <button className={classes.button}>
+const HeaderCartButton = props => {
+    return <button className={classes.button} onClick={props.onClick}>
         <span className={classes.icon}>
             <CartIcon />
         </span>
